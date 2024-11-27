46d172be3dbed6622dd327552cd9505f7d96b5a7
ansango <anibalsantosgo@gmail.com>
2024-11-27 02:12:25 +0100

    refactor: change POST handler to GET and remove unused track scrobble logic

--- src/routes/track/+server.ts
+++ src/routes/track/+server.ts
@@ -1,7 +1,7 @@
-import { trackApiMethods, userApiMethods } from '$lib/lastfm/services';
+import { userApiMethods } from '$lib/lastfm/services';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
-export const POST: RequestHandler = async ({ cookies }) => {
+export const GET: RequestHandler = async ({ cookies }) => {
 	const session = cookies.get('session');
 	const user = cookies.get('user');
 
@@ -14,44 +14,4 @@ export const POST: RequestHandler = async ({ cookies }) => {
 	} = await userApiMethods.getRecentTracks({ user });
 
 	return json(track, { status: 200 });
-};
-
-export const GET: RequestHandler = async ({ cookies }) => {
-	const session = cookies.get('session');
-	if (!session) {
-		return json({ error: 'Session not found' }, { status: 404 });
-	}
-	const sk = session;
-	const artist = 'Mogwai';
-	const track = 'Yes! I Am A Long Way From Home';
-	const album = 'Young Team (Deluxe Edition)';
-	const timestamp = Math.floor(Date.now() / 1000).toString();
-
-	// const response = await trackApiMethods.postTrackScrobble({
-	// 	artist,
-	// 	track,
-	// 	timestamp,
-	// 	sk,
-	// 	album
-	// });
-
-	const response = await trackApiMethods.postBatchTrackScrobble({
-		tracks: [
-			{
-				artist,
-				track,
-				timestamp,
-				album
-			},
-			{
-				artist: 'Pink Floyd',
-				track: 'Time',
-				timestamp: Math.floor(Date.now() / 1000).toString(),
-				album: 'The Dark Side of the Moon'
-			}
-		],
-		sk
-	});
-
-	return json(response, { status: 200 });
 };
